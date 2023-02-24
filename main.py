e2b0bca2c16ccc0cbfed4c2fd0e9e088222eae08
shebov3 <shehabtarik425@yahoo.com>
2023-02-24 11:36:53 +0200

    dadg

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 import re
 text = input("Write your mathematicial equation:")
 
-# searches for digits
-search = re.search("\d+",text)
-digit_from_text = search.group(0)
+def function():
+    global text
+    search = re.search("\d+",text) # search for digits
+    if search:
+        num = search.group(0)
+        search2 = re.search("\W",text) # search for operator that are after digits like + - / * %
+        if search2:
+            operator = search2.group(0)
+            # removing the first digits from the text
+            text = text[search2.span(0)[1]:]
+            print(text)
+            function()
 
-# removing the first digits from the text
-text = text[search.span(0)[1]:]
-print(text)
+function()

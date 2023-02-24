bb57126bcaa67b5a8796dcf7bbfb88c9443df3ce
shebov3 <shehabtarik425@yahoo.com>
2023-02-24 11:56:46 +0200

    done

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 # Shehab Tarek 320220099    
 import re
+print("you can use + - / * % operators only for now\n")
 text = input("Write your mathematicial equation:")
+
+text.replace(" ", "")# removing all spaces from the equation
 
 def function():
     global text
-    search = re.search("\d+",text) # search for digits
+    search = re.search("\d+",text) # search for digits 0 ~ 9
     if search:
-        num = search.group(0)
-        search2 = re.search("\W",text) # search for operator that are after digits like + - / * %
+        num = int(search.group(0))
+        search2 = re.search("\W",text) # search for operators like + - / * %
         if search2:
             operator = search2.group(0)
-            # removing the first digits from the text
+            # removing the first few digits and first operator from the text
             text = text[search2.span(0)[1]:]
             print(text)
-            function()
 
-function()
+            if operator == "+":
+                return num + function()
+            elif operator == '-':
+                return num - function()
+            elif operator == '*':
+                return num * function()
+            elif operator == '/':
+                return num / function()
+            elif operator == '%':
+                return num % function()
+            else: # none of defined operators were found
+                return num
+        else: # no operator found after digits
+            return num
+
+print(function())

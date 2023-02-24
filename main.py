f18d2c1d61c6f4153a37cf5192e20106c4ceff3b
shebov3 <shehabtarik425@yahoo.com>
2023-02-24 11:10:17 +0200

    asd

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# Shehab Tarek 320220099
+# Shehab Tarek 320220099    
 import re
-
 text = input("Write your mathematicial equation:")
 
-print(re.search("\d+",text).group(0))
+# searches for digits
+search = re.search("\d+",text)
+digit_from_text = search.group(0)
+
+# removing the first digits from the thing
+text = text[search.span(0)[1]:]
+print(text)
